d2950551bd22f0ad48be45c5bd91eb35b94348ac
kumaraakarshan <aakarshan0005@gmail.com>
2023-10-04 20:12:16 +0530

    2nd commit

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,11 +1,20 @@
-const Greeting = ()=>{
+import { useState } from "react";
+
+const Greeting = () => {
+    const [changedText, setChangedText] = useState(false)
+
+    const changedTextHandler = () => {
+        setChangedText(true);
+    }
     
     return (
         <div>
             <h2>
                 Hello world
             </h2>
-            <p>it's good to see you</p>
+            {!changedText && <p>it's good to see you</p>}
+            {changedText && <p> changed</p>}
+            <button onClick={changedTextHandler}>Change Text</button>
         </div>
     )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,6 +1,7 @@
 import Greeting from "./Greeting"
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
-test('render Hello Worls as a text', () => {
+test('render Hello World as a text', () => {
 
     //arrange
     render(<Greeting />);
@@ -13,4 +14,32 @@ test('render Hello Worls as a text', () => {
     expect(helloWorldElement).toBeInTheDocument();
 
 
+});
+
+test(' renders good to see if the button was not clicked', () => {
+    render(<Greeting />)
+
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+})
+
+test(' renders if the button was clicked', () => {
+    render(<Greeting />)
+
+    const buttonElement = screen.getByRole('button')
+
+    userEvent.click(buttonElement)
+
+    const outputElement = screen.getByText('changed', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+})
+test(' doesnt renders good to see you  if the button was clicked', () => {
+    render(<Greeting />)
+
+    const buttonElement = screen.getByRole('button')
+
+    userEvent.click(buttonElement)
+
+    const outputElement = screen.queryByText('good to see you', { exact: false });
+    expect(outputElement).toBeNull();
 })
